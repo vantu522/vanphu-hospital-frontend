b1e8a97e1e9f943ee20ab5a6824cf9419ed0d437
tudgvn23 <tuduongvan2305@gmail.com>
2025-04-16 09:42:56 +0700

    done button

--- src/components/client/layout/Header.jsx
+++ src/components/client/layout/Header.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {uh}from '../../../assets/images/dvt.png'
 
 const Header = () => {
   return (
     <header style={{ padding: "1rem", background: "#f0f0f0" }}>
-      <nav>
-        <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
-        <Link to="/dashboard">Dashboard</Link>
-      </nav>
+      <div className="flex gap-12">
+        <div className="logo"></div>
+        <div className="header-right">
+          <div className="header-top">
+            <div className="bg">
+              <img src={uh} alt=""  className="w-2.5"/>
+            </div>
+          </div>
+          <div className="header-bot"></div>
+        </div>
+      </div>
     </header>
   );
 };
