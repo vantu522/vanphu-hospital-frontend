439dbaaeeeb06a95226fbe97faf78300a5e663ed
Hungvu0712 <122802824+Hungvu0712@users.noreply.github.com>
2025-04-16 12:01:06 +0700

    feat: slider

--- src/components/client/layout/ClientLayout.jsx
+++ src/components/client/layout/ClientLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./header";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import Slider from "../Slider";
 const ClientLayout = () => {
   return (
     <>
       <Header />
+      <Slider />
       {/* <main className="p-1">
         <Outlet />
       </main> */}
